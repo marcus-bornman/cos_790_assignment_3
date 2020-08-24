9ebe0388f2220aa3d686d386361f985d89dd8218
marcus-bornman <marcus.bornman@frei.one>
2020-08-24 23:37:15 +0200

    Polished main method

--- src/timetabling/Main.java
+++ src/timetabling/Main.java
@@ -1,5 +1,8 @@
 package timetabling;
 
+import genalg.GenAlg;
+import initialsoln.InitialSoln;
+import timetabling.evohyp.HeuristicApplier;
 import timetabling.utils.ProblemReader;
 
 import java.io.IOException;
@@ -9,8 +12,26 @@ public class Main {
 		String filePath = "problems/exam_comp_set1.exam";
 
 		try {
-			Problem problem = ProblemReader.problemFromFile(filePath);
-			System.out.println("Success, WHOOP WHOOP!");
+			long seed = System.currentTimeMillis();
+			String heuristics = HeuristicApplier.SUPPORTED_HEURISTICS;
+			GenAlg genAlg = new GenAlg(seed, heuristics);
+			genAlg.setPopulationSize(100);
+			genAlg.setTournamentSize(10);
+			genAlg.setNoOfGenerations(100);
+			genAlg.setMutationRate(0.1);
+			genAlg.setCrossoverRate(0.1);
+			genAlg.setInitialMaxLength(10);
+			genAlg.setOffspringMaxLength(50);
+			genAlg.setMutationLength(5);
+			genAlg.setProblem(ProblemReader.problemFromFile(filePath));
+
+			InitialSoln solution = genAlg.evolve();
+
+			System.out.println("Best Solution");
+			System.out.println("--------------");
+			System.out.println("Fitness: " + solution.getFitness());
+			System.out.println("Heuristic combination: " + solution.getHeuCom());
+			System.out.println("Solution: \n" + solution);
 		} catch (IOException e) {
 			System.out.println("File at path `" + filePath + "` could not be found.");
 		}
