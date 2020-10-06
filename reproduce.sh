d726e018c5e4c6dc5924c41f1d64089fb10d699f
marcus-bornman <marcus.bornman@frei.one>
2020-10-06 19:10:36 +0200

    Changed problem sets to run experiments on

--- reproduce.sh
+++ reproduce.sh
@@ -4,29 +4,29 @@ mvn clean compile assembly:single
 for ((num = 1; num <= 10; num++)); do
   echo "Test run $num executing on problems/exam_comp_set1.exam using Single-Point Search..."
   java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set1.exam -r $num -o results/exam_comp_set1/run_$num.singlepoint
-  echo "Test run $num executing on problems/exam_comp_set2.exam using Single-Point Search..."
-  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set2.exam -r $num -o results/exam_comp_set2/run_$num.singlepoint
-  echo "Test run $num executing on problems/exam_comp_set5.exam using Single-Point Search..."
-  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set5.exam -r $num -o results/exam_comp_set5/run_$num.singlepoint
+  echo "Test run $num executing on problems/exam_comp_set4.exam using Single-Point Search..."
+  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set4.exam -r $num -o results/exam_comp_set4/run_$num.singlepoint
   echo "Test run $num executing on problems/exam_comp_set6.exam using Single-Point Search..."
   java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set6.exam -r $num -o results/exam_comp_set6/run_$num.singlepoint
+  echo "Test run $num executing on problems/exam_comp_set8.exam using Single-Point Search..."
+  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set8.exam -r $num -o results/exam_comp_set8/run_$num.singlepoint
   echo "Test run $num executing on problems/exam_comp_set9.exam using Single-Point Search..."
   java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set9.exam -r $num -o results/exam_comp_set9/run_$num.singlepoint
-  echo "Test run $num executing on problems/exam_comp_set10.exam using Single-Point Search..."
-  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set10.exam -r $num -o results/exam_comp_set10/run_$num.singlepoint
+  echo "Test run $num executing on problems/exam_comp_set12.exam using Single-Point Search..."
+  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s singlepoint -p problems/exam_comp_set12.exam -r $num -o results/exam_comp_set12/run_$num.singlepoint
 
   echo "Test run $num executing on problems/exam_comp_set1.exam using Multi-Point Search..."
   java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set1.exam -r $num -g genetic.parameters -o results/exam_comp_set1/run_$num.multipoint
-  echo "Test run $num executing on problems/exam_comp_set2.exam using Multi-Point Search..."
-  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set2.exam -r $num -g genetic.parameters -o results/exam_comp_set2/run_$num.multipoint
-  echo "Test run $num executing on problems/exam_comp_set5.exam using Multi-Point Search..."
-  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set5.exam -r $num -g genetic.parameters -o results/exam_comp_set5/run_$num.multipoint
+  echo "Test run $num executing on problems/exam_comp_set4.exam using Multi-Point Search..."
+  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set4.exam -r $num -g genetic.parameters -o results/exam_comp_set4/run_$num.multipoint
   echo "Test run $num executing on problems/exam_comp_set6.exam using Multi-Point Search..."
   java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set6.exam -r $num -g genetic.parameters -o results/exam_comp_set6/run_$num.multipoint
+  echo "Test run $num executing on problems/exam_comp_set8.exam using Multi-Point Search..."
+  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set8.exam -r $num -g genetic.parameters -o results/exam_comp_set8/run_$num.multipoint
   echo "Test run $num executing on problems/exam_comp_set9.exam using Multi-Point Search..."
   java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set9.exam -r $num -g genetic.parameters -o results/exam_comp_set9/run_$num.multipoint
-  echo "Test run $num executing on problems/exam_comp_set10.exam using Multi-Point Search..."
-  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set10.exam -r $num -g genetic.parameters -o results/exam_comp_set10/run_$num.multipoint
+  echo "Test run $num executing on problems/exam_comp_set12.exam using Multi-Point Search..."
+  java -jar target/cos_790_assignment_2-1.0-SNAPSHOT-jar-with-dependencies.jar -s multipoint -p problems/exam_comp_set12.exam -r $num -g genetic.parameters -o results/exam_comp_set12/run_$num.multipoint
 done
 
 echo "Success! All tests have been completed successfully. See the 'results' folder for all output files."
