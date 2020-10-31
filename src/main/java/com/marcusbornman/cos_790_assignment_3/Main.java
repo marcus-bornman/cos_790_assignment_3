72b703537b3f3a86aa6af987f3a3e02e07da9667
marcus-bornman <marcus.bornman@frei.one>
2020-10-31 09:15:13 +0200

    Added dependency on org.moeaframework.problem.tsplib

--- src/main/java/com/marcusbornman/cos_790_assignment_3/Main.java
+++ src/main/java/com/marcusbornman/cos_790_assignment_3/Main.java
@@ -1,7 +1,12 @@
 package com.marcusbornman.cos_790_assignment_3;
 
-public class Main {
-	public static void main(String[] args) {
+import org.moeaframework.problem.tsplib.TSPInstance;
 
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		TSPInstance problem = new TSPInstance(new File("./data/tsp/pcb442.tsp"));
 	}
 }
